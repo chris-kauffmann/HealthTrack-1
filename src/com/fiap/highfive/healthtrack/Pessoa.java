45d937513da070d043a3e58533b0d14cc3e96d1e
nakasonemarcelo <69497169+nakasonemarcelo@users.noreply.github.com>
2021-08-29 15:49:13 -0300

    Primeira commit Classe pessoa

---
+++ src/com/fiap/highfive/healthtrack/Pessoa.java
@@ -0,0 +1,16 @@
+package com.fiap.highfive.healthtrack;
+
+public abstract class Pessoa {
+
+    // Abstrai características de pessoa
+    private String nome;
+    private int cpf;
+    private String sexo;
+    private String endereco;
+    private int nroEnd;
+    private int cep;
+    private String eMail;
+    private int tel;
+    private int celular;
+
+}
